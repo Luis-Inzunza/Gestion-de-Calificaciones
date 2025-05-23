9fd0dab48473c90eddaa627df515fae757d04528
Luis Alejandro Cruz Inzunza <cruzalejandro113@gmail.com>
2025-05-22 21:53:12 -0600

    feat: agregamos el apartado de generador de reportes

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $nombre = $_SESSION['nombre'] ?? 'Usuario Fantasma';
     <div class="text-lg font-semibold">
       Bienvenido, <?= htmlspecialchars($nombre) ?>
     </div>
-    <a href="login/logout.php" class="bg-red-500 px-3 py-1 rounded hover:bg-red-600">
+    <a href="./pages/login/logout.php" class="bg-red-500 px-3 py-1 rounded hover:bg-red-600">
       Cerrar Sesión
     </a>
   </header>
@@ -48,6 +48,13 @@ $nombre = $_SESSION['nombre'] ?? 'Usuario Fantasma';
     <h2 class="text-xl font-bold mb-4">Asignaturas</h2>
     <img src="./images/asignaturas.png" alt="asignaturas" class="w-32 h-32 mb-4">
     <a href="./pages/asignaturas.php" class="text-blue-600 underline">Ir a Asignaturas</a>
+  </div>
+
+    <!-- Reportes -->
+  <div class="bg-white shadow-lg rounded-xl p-6 flex flex-col items-center">
+    <h2 class="text-xl font-bold mb-4">Generar Reportes</h2>
+    <img src="./images/reporte.png" alt="asignaturas" class="w-32 h-32 mb-4">
+    <a href="./pages/reporte.php" class="text-blue-600 underline">Ir a Reportes</a>
   </div>
 </section>
 
